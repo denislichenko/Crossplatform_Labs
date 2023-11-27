78178d9a79b66486ced64d6c6a39c4a9766b128c
Denys Lichenko <vip.lichenko@gmail.com>
2023-11-27 20:31:13 +0200

    Lab4: Add Vagrant Files Lab5: Init solution

---
+++ Lab5/Lab5_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab5_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
